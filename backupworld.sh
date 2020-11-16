da3ac7a3e1352a6517ff3d61e417703b31010faf
Lunarixus <Nathan@lunarixus.party>
2020-11-16 09:35:15 +0000

    backupworld: Avoid backing up instantly when script is ran
    Signed-off-by: Lunarixus <Nathan@lunarixus.party>

--- backupworld.sh
+++ backupworld.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+count=0
+
 while [ : ]
 do
     date=$(date "+%d/%m/%y")
@@ -7,13 +9,17 @@ do
 
     if pgrep -x "java" >/dev/null
     then
-        echo "Server is running!"
+        if [ "$count" -eq "0" ]; then
+            then
+                echo "Script has been ran for the first time, skipping backup."
+            else
+                echo "Server is running!"
+                echo "Starting backup on day $date at time $time"
 
-        echo "Starting backup on day $date at time $time"
-
-        git add world/ logs/ crash-reports/
-        git commit -s -m "$date: Backup of server at $time"
-        git push -f
+                git add world/ logs/ crash-reports/
+                git commit -s -m "$date: Backup of server at $time"
+                git push -f
+        fi
     else
         echo "Server is not running!"
         echo "Skipping backup on day $date at time $time"
